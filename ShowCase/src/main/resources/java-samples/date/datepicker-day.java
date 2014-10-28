9cb51a2afc9c11e02908fe77febc4ce5c480dbcc
a_baymusaeva <a.baymusaeva@ARTA>
2014-10-28 09:40:10 +0600

    комментарий к примерам календаря

--- ShowCase/src/main/resources/java-samples/date/datepicker-day.java
+++ ShowCase/src/main/resources/java-samples/date/datepicker-day.java
@@ -4,8 +4,10 @@ import kz.arta.synergy.components.client.theme.ColorType;
 public class Sample {
     public static void main(String[] args) {
         // по умолчание режима выбора - день
+        /*светлый календарь*/
         ArtaDatePicker datePicker = new ArtaDatePicker();
 
+        /*темный календарь*/
         ArtaDatePicker datePickerDark = new ArtaDatePicker(ColorType.BLACK);
     }
 }
